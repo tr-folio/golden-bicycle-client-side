b234eb30ef2275b42958e2b9b0a566ebe362e507
tr-folio <tahrimurmoon@gmail.com>
2021-11-11 23:54:00 +0600

    some bugs fixed

--- src/Pages/Home/Products/Product/Product.js
+++ src/Pages/Home/Products/Product/Product.js
@@ -6,14 +6,14 @@ const Product = (props) => {
 
     return (
         <Col xs="12" md="6" lg="4" className="mb-3">
-            <h6>{product.productName}</h6>
+            <h6 className="fs-4 bg-dark m-0 text-light py-2">{product.productName}</h6>
             <p>
                 <img src={product.img} alt="" className="img-fluid"/>
             </p>
             <p>
                 {product.description}
             </p>
-            <Button variant="primary">Purchase</Button>
+            <Button variant="primary" className="w-100">Purchase</Button>
         </Col>
     );
 }
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
                         <h3 className="text-info text-start mb-3">Golden Bicycle</h3>
                         <p className="text-light" style={{textAlign: 'justify'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <p className="text-info text-start">
-                            <i className="fab fa-facebook"></i>
-                            <i className="fab fa-instagram"></i>
-                            <i className="fab fa-twitter"></i>
+                            <i className="fab fa-facebook fs-3 me-3"></i>
+                            <i className="fab fa-instagram fs-3 me-3"></i>
+                            <i className="fab fa-twitter fs-3 me-3"></i>
                         </p>
                     </Col>
                     <Col xs="12" lg="4">
@@ -24,9 +24,9 @@ const Footer = () => {
                         <Link style={{textDecoration: 'none'}} className="text-info" to="">Link3</Link> <br/> <br/>
                     </Col>
                     <Col xs="12" lg="4">
-                        <h3 className="text-light text-lg-info text-lg-end mb-3">Contact Us</h3>
-                        <p className="text-info text-lg-light text-lg-end">Email: abc@example.com</p>
-                        <p  className="text-info text-lg-light text-lg-end">Phone: 22222222</p>
+                        <h3 className="text-light text-lg-end mb-3">Contact Us</h3>
+                        <p className="text-info text-lg-end">Email: abc@example.com</p>
+                        <p  className="text-info text-lg-end">Phone: 22222222</p>
                     </Col>
                 </Row>
             </Container>
