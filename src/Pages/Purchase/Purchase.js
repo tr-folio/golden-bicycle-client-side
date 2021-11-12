7660354fec595c904d48f80d5cf2e203929f77bb
tr-folio <tahrimurmoon@gmail.com>
2021-11-12 16:08:30 +0600

    selected product loaded into purchase page

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Header from '../Shared/Header/Header';
 
 const Purchase = () => {
+    const [selectedProduct, setSelectedProduct] = useState({});
     const { id } = useParams();
-    console.log(id);
+    
+    useEffect(() => {
+        fetch(`http://localhost:5000/readSingleProduct/${id}`)
+        .then(res => res.json())
+        .then(data => setSelectedProduct(data[0]));
+    }, [id]);
+
     return (
         <div>
             <Header></Header>
-            <h2>This is Purchase page</h2>
+            <h2>{selectedProduct.productName}</h2>
         </div>
     );
 };
