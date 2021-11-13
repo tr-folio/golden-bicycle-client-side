ded91485192f5e028703c998e542695a99ab66a6
tr-folio <tahrimurmoon@gmail.com>
2021-11-14 01:09:29 +0600

    some bugs of dashboard home fixed

--- src/Pages/DashBoard/DHome/DashboardHome.js
+++ src/Pages/DashBoard/DHome/DashboardHome.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import Header from '../../Shared/Header/Header';
+import './DashboardHome.css';
 
 const DashboardHome = () => {
+    const sideBar = document.getElementById('side-bar');
+
+    const showSideBar = () => {
+        sideBar.style.display = 'block';
+    }
+    const hideSideBar = () => {
+        sideBar.style.display = 'none';
+    }
+
     return (
         <div>
             <Header></Header>
+            <div className="mb-3 bg-dark py-1">
+                <Container className="d-flex justify-content-between">
+                    <Button variant="secondary" onClick={showSideBar}>Show Sidebar</Button>
+                    <Button variant="secondary" onClick={hideSideBar} className="d-lg-none">Hide Sidebar</Button>
+                </Container>
+            </div>
             <br/>
-            <h2>This is Dashboard Home</h2>
+            <Container>
+                <Row>
+                    <Col xs="12" lg="3" id="side-bar" className="custom-columns">
+                        Side bar
+                    </Col>
+                    <Col xs="12" lg="9">
+                        Dashboard Contents
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
