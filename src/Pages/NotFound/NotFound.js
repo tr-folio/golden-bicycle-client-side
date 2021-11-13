16f1289f3db7201d91dab0655b4262d665ac64ce
tr-folio <tahrimurmoon@gmail.com>
2021-11-14 00:12:30 +0600

    some bugs fixed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from '../Shared/Header/Header';
 
 const NotFound = () => {
@@ -7,6 +8,9 @@ const NotFound = () => {
             <Header></Header>
             <br/>
             <h2>Sorry! The page is not found.</h2>
+            <Container>
+                <img src="https://i.ibb.co/B6DgXf9/sorry-3160426.png" alt="" className="img-fluid"/>
+            </Container>
         </div>
     );
 };
