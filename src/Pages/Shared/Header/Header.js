c6aee95ebc51d98994c409049c96089d93b9e34d
tr-folio <tahrimurmoon@gmail.com>
2021-11-13 15:00:37 +0600

    logout working

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
+import useAuth from "../../../hooks/useAuth";
 
 const Header = () => {
+    const { user, logout } = useAuth();
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -12,7 +14,8 @@ const Header = () => {
                     <Nav>
                         <Nav.Link as={HashLink} to="/home">Home</Nav.Link>
                         <Nav.Link as={HashLink} to="/explore">Explore</Nav.Link>
-                        <Nav.Link as={HashLink} to="">Link2</Nav.Link>
+                        {!user?.email && <Nav.Link as={HashLink} to="/login">Login</Nav.Link>}
+                        {user?.email && <Button onClick={logout} variant="primary">Logout</Button>}
                         <Nav.Link as={HashLink} to="">Link3</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
