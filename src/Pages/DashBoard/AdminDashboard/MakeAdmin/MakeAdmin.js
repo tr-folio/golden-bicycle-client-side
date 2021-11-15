b1256b8da96d4da416d480152aeb766d4c3d0777
tr-folio <tahrimurmoon@gmail.com>
2021-11-15 22:49:10 +0600

    made admin successful

--- src/Pages/DashBoard/AdminDashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/DashBoard/AdminDashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const MakeAdmin = () => {
+    let emailField = '';
+    let email = '';
+
+    const handleMakeAdmin = (event) => {
+        event.preventDefault();
+        emailField = document.getElementById('email');
+        email = emailField.value;
+        // console.log(email);
+        const user = {email: email};
+        fetch('http://localhost:5000/makeAnAdmin', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            if (data.acknowledged) {
+                window.alert('Made admin successful');
+            }
+        })
+    }
     return (
         <div>
-            <h2>This is Make Admin</h2>
+            <h2 className="text-start">Make An Admin</h2>
+            <br/>
+            <form onSubmit={handleMakeAdmin}>
+                <div className="text-start"><input type="email" id="email" placeholder="Enter Email" className="w-75 px-2 fs-5"/></div>
+                <br/>
+                <div className="text-start">
+                    <Button onClick={handleMakeAdmin} variant="primary" type="button">Make Admin</Button>
+                </div>
+            </form>
         </div>
     );
 };
