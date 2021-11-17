2e19868d2950e3aef7d98c1cb07ec2c8eeb4fa44
tr-folio <tahrimurmoon@gmail.com>
2021-11-17 09:04:22 +0600

    shipped request send to changeStatus api successfully

--- src/Pages/DashBoard/AdminDashboard/ManageAllOrders/ManageOrder/ManageOrder.js
+++ src/Pages/DashBoard/AdminDashboard/ManageAllOrders/ManageOrder/ManageOrder.js
@@ -3,6 +3,29 @@ import { Button } from 'react-bootstrap';
 
 const ManageOrder = (props) => {
     const { order } = props;
+
+    const changeToShipped = () => {
+        // window.alert('shipped');
+        fetch('http://localhost:5000/changeStatus', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({id: order._id})
+        })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            if (data.acknowledged) {
+                window.alert('status changed successfully');
+                window.location.reload();
+            }
+        })
+    }
+
+    const deleteProduct = () => {
+        window.alert('delete product');
+    }
     
     return (
         <tr>
@@ -12,8 +35,8 @@ const ManageOrder = (props) => {
             <td>{order.address}</td>
             <td>{order.phone}</td>
             <td>{order.status}</td>
-            <td><Button variant="primary">Shipped</Button></td>
-            <td><Button variant="danger">Delete</Button></td>
+            <td><Button variant="primary" onClick={changeToShipped}>Shipped</Button></td>
+            <td><Button variant="danger" onClick={deleteProduct}>Delete</Button></td>
         </tr>
     );
 };
