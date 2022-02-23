caeca37fa60cd2c983fb7f94e484a0319c33d30f
tr-folio <tahrimurmoon@gmail.com>
2022-02-23 15:10:33 +0600

    some bugs solved

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,7 @@ import Footer from "../../Shared/Footer/Footer";
 import Header from "../../Shared/Header/Header";
 import Banner from "../Banner/Banner";
 import CarbonBicycle from "../CarbonBicycle/CarbonBicycle";
-import Technology from "../NewTechnology.js/technology";
+import Technology from "../NewTechnology/technology";
 import Products from "../Products/Products";
 import Reviews from "../Reviews/Reviews";
 import Stories from "../Stories/Stories";
